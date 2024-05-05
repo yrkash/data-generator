2a12aee6255b274a4af8dcc0ac9b7796b6ab2597
yrkash <yrkash@gmail.com>
2024-05-05 15:02:03 +0300

    first_kafka

---
+++ src/main/java/com/example/datageneratormicroservice/service/KafkaDataService.java
@@ -0,0 +1,11 @@
+package com.example.datageneratormicroservice.service;
+
+import com.example.datageneratormicroservice.model.Data;
+
+public interface KafkaDataService {
+
+    void send(Data data);
+
+
+
+}
