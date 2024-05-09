35918364b409c41178ba73c66adaec9a4bf87f3e
yrkash <yrkash@gmail.com>
2024-05-09 20:21:11 +0300

    first microservice realized: 2 API to put data to kafka (send one measurement + random sending every 3 seconds). Reactive with Mono

---
+++ src/main/java/com/example/datageneratormicroservice/model/test/DataTestOptions.java
@@ -0,0 +1,15 @@
+package com.example.datageneratormicroservice.model.test;
+
+import com.example.datageneratormicroservice.model.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class DataTestOptions {
+
+    private int delayInSeconds;
+    private Data.MeasurementType[] measurementTypes;
+}
